de3326061e82df492f9e002a0e88a449263a5a41
teru27 <shinohara727teruhisa@icloud.com>
2023-02-24 21:55:04 +0900

    配列の回転

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # recat-Next
 
 最新の react 環境
+npm ci  
+npm run dev
--- utli/generalSrc.ts
+++ utli/generalSrc.ts
@@ -12,10 +12,11 @@ export function dimensional2Array(
 ): string[][] | number[][] {
   let board: string[][] | number[][] = new Array();
   const indexRow = index2 !== undefined ? index2 : index;
+  const numOrStr = arrNum ? 0 : "0";
   for (var i = 0; i < index; i++) {
     board[i] = new Array();
     for (var j = 0; j < indexRow; j++) {
-      board[i][j] = arrNum ? 0 : "0";
+      board[i][j] = numOrStr;
     }
   }
   return board;
@@ -50,4 +51,45 @@ export function rotationMatrix(
  */
 export function radian2Angle(angle: number) {
   return angle * (Math.PI / 180);
+}
+
+/**
+ * 正方形の二次元配列の回転
+ * @param array 配列
+ * @param direction 回転方向
+ * @returns
+ */
+export function rotate2Array(
+  array: string[][] | number[][],
+  direction: "l" | "r"
+): string[][] | number[][] {
+  const xLength = array.length;
+  const yLength = array[0].length;
+  let rotate2Array: string[][] | number[][] = dimensional2Array(
+    yLength,
+    false,
+    xLength
+  );
+  switch (direction) {
+    // 左回転
+    case "l":
+      array.map((line, i) => {
+        line.map((cell, j) => {
+          rotate2Array[array.length - 1 - i][array.length - 1 - j] =
+            array[array.length - 1 - j][i];
+        });
+      });
+      break;
+    //右回転
+    case "r":
+      array.map((line, i) => {
+        return line.map((cell, j) => {
+          return (rotate2Array[array.length - 1 - j][array.length - 1 - i] =
+            array[i][array.length - 1 - j]);
+        });
+      });
+      break;
+  }
+
+  return rotate2Array;
 }
