fbc017e124a952ded73a6b3cd16bb9d14d0f26d2
teru27 <shinohara727teruhisa@icloud.com>
2023-08-19 00:42:16 +0900

    fix .sh

--- #!/bin/bash/vercel-ignore-build-step.sh
+++ #!/bin/bash/vercel-ignore-build-step.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
-
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "main" ]] ; then
+if [[  "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
   # Proceed with the build
-  echo "✅ - Build can proceed"
+    echo "✅ - Build can proceed"
   exit 1;
 
 else
