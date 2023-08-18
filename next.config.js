ffa9683e626825a5715c2834b469e89e509c0914
teru27 <shinohara727teruhisa@icloud.com>
2023-08-18 16:40:02 +0900

    環境別でベースんパス変更

--- next.config.js
+++ next.config.js
@@ -4,18 +4,23 @@
 
 const isProd = process.env.NODE_ENV === "production";
 
-const prefixPath = !isProd ? "/recat-Next" : "";
+const prefixPath =
+  isProd && process.env.NEXT_PUBLIC_PREFIC_PATH
+    ? process.env.NEXT_PUBLIC_PREFIC_PATH
+    : "";
+
+console.log(prefixPath === "");
 
 const nextConfig = {
   /* config options here */
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: "/",
+  assetPrefix: prefixPath,
   basePath: prefixPath,
   trailingSlash: true,
-  compiler: {
-    removeConsole: isProd,
-  },
+  // compiler: {
+  //   removeConsole: isProd,
+  // },
 };
 
 module.exports = nextConfig;
