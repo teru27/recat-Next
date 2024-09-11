0815fabc9c57f0ad577df1f70d035f5b74ea8b8c
teru27 <shinohara727teruhisa@icloud.com>
2024-09-11 13:06:54 +0900

    メニューの判定処理の修正

--- src/pages/calcio/index.tsx
+++ src/pages/calcio/index.tsx
@@ -1,102 +1,173 @@
+import { Root2, cardData, cardInfo, menuInfo, data2 } from "@util/data";
 import React, { useEffect, useState } from "react";
 
-import { tanStackGetData } from "../../util/gas-api";
-import { Todo, card, cardsType, menuDataType } from "../../util/types";
-
-import styles from "./calcio.module.scss";
-import { useQuery } from "@tanstack/react-query";
-import { cardData, menuData } from "@component/calcio/calcioData";
-import { CalcioCardRow } from "@component/calcio/calcioCardRow";
-
-const menuDatas: menuDataType[] = menuData;
-
-const cardDatas: cardsType = cardData;
-
 export default function Home() {
-  const [getCardDatas, setCardDatas] = useState<cardsType>(cardDatas);
-  const [getMenuData, setMenuData] = useState<menuDataType[]>([]);
-  const [getSelectCard, setSelectCard] = useState<string[]>([]);
+  const [list, setList] = useState<cardInfo[]>([]);
+  const [menuList, setMenuList] = useState<Root2[]>([]);
+  const [cardData, setCardData] = useState<cardData>(data2);
+  const [get, set] = useState<boolean>(false);
 
-  const addSelectCard = (card: card) => {
-    if (!getSelectCard.includes(card.key)) {
-      setSelectCard([...getSelectCard, card.key]);
-    }
+  const checkbox = (card: cardInfo) => {
+    const newCardData = cardData.map((cardInfos) => {
+      const idx = cardInfos.findIndex(
+        (cardInfo) => cardInfo.value == card.value
+      );
+      if (idx != -1) {
+        cardInfos[idx].status = !card.status;
+        changList(cardInfos[idx]);
+      }
 
-    // カウント上下操作予定
+      return cardInfos;
+    });
+    makeList();
+    setCardData(newCardData);
   };
 
-  // 検索機能
+  const allCheckChang = (status: boolean) => {
+    const newList: cardInfo[] = [];
+    const newCardData = cardData.map((cardInfos) => {
+      cardInfos.map((cardInfo) => {
+        cardInfo.status = status;
+        if (!status) {
+          setList([]);
+        } else {
+          newList.push(cardInfo);
+        }
+      });
+      return cardInfos;
+    });
+    setList(newList);
+    setCardData(newCardData);
+  };
+
+  const changList = (card: cardInfo) => {
+    const newList: cardInfo[] = [...list];
+    if (card.status) {
+      newList.push(card);
+      setList(newList);
+    } else {
+      const newList = list.filter((n) => n.value != card.value);
+      setList(newList);
+    }
+  };
+
+  const makeList = () => {
+    console.log(list);
+
+    if (list.length == 0) {
+      setMenuList([]);
+      return;
+    }
+    const menuListData: Root2[] = [];
+    list.forEach((card1: cardInfo, i) => {
+      list.forEach((card2: cardInfo, j) => {
+        const select = menuInfo.find(
+          (menu) =>
+            menu.card1 == card1.value &&
+            menu.card2 == card2.value &&
+            menu.card3 == ""
+        );
+        if (select != undefined) {
+          menuListData.push(select);
+        }
+
+        list.forEach((card3: cardInfo, j) => {
+          const select = menuInfo.find(
+            (menu) =>
+              menu.card1 == card1.value &&
+              menu.card2 == card2.value &&
+              menu.card3 == card3.value
+          );
+          if (select != undefined) {
+            menuListData.push(select);
+          }
+        });
+      });
+    });
+
+    setMenuList(menuListData);
+  };
+
   useEffect(() => {
-    setMenuData(menuDatas);
-    console.log(getSelectCard);
-  }, [getSelectCard]);
+    makeList();
+  }, [list]);
 
   return (
-    <div className={styles.main}>
-      <h1>カード一覧</h1>
-      <div className={styles.container}>
-        <div className={styles.cardBox}>
-          <CalcioCardRow
-            cardData={getCardDatas.tactical}
-            clickEvent={addSelectCard}
-          />
+    <div>
+      <button onClick={() => allCheckChang(true)}>全て埋める</button>
+      <button onClick={() => allCheckChang(false)}>全て外す</button>
+      <div style={{ display: "flex" }}>
+        {cardData.map((cardInfo, i) => (
+          <div key={`key_` + i}>
+            {cardInfo.map((card, j) => (
+              <div
+                style={{ display: "flex" }}
+                key={`key_` + j}
+                onClick={() => checkbox(card)}
+              >
+                <input
+                  type="checkbox"
+                  value={card.value}
+                  onChange={() => {}}
+                  checked={card.status}
+                />
+                {card.value}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+      <div style={{ display: "flex" }}>
+        <div style={{ margin: "20px" }}>
+          <h2>選択したメニュー</h2>
+          {list.map((card, i) => (
+            <div style={{ display: "flex" }} key={`key_` + i}>
+              {card.value}
+            </div>
+          ))}
         </div>
-        <div className={styles.cardBox}>
-          <CalcioCardRow
-            cardData={getCardDatas.technical}
-            clickEvent={addSelectCard}
-          />
-        </div>
-        <div className={styles.cardBox}>
-          <CalcioCardRow
-            cardData={getCardDatas.physical}
-            clickEvent={addSelectCard}
-          />
-        </div>
-        <div className={styles.cardBox}>
-          <CalcioCardRow
-            cardData={getCardDatas.support}
-            clickEvent={addSelectCard}
-          />
+        <div>
+          <div style={{ margin: "40px" }}>
+            <h2>スペシャルメニュー</h2>
+          </div>
+          <div style={{ display: "flex", margin: "30px" }} key={`key_`}>
+            <table border={1}>
+              <tbody>
+                <tr>
+                  <th>練習名</th>
+                  <th>キック</th>
+                  <th>スピード</th>
+                  <th>体力</th>
+                  <th>テクニック</th>
+                  <th>フィジカル</th>
+                  <th>ジャンプ</th>
+                  <th>メンタル</th>
+                  <th>疲労度</th>
+                  <th>タイプ1</th>
+                  <th>タイプ2</th>
+                  <th>タイプ3</th>
+                </tr>
+                {menuList.map((card, i) => (
+                  <tr key={i}>
+                    <td>{card.menu}</td>
+                    <td>{card.kick}</td>
+                    <td>{card.speed}</td>
+                    <td>{card.energy}</td>
+                    <td>{card.technique}</td>
+                    <td>{card.physical}</td>
+                    <td>{card.jump}</td>
+                    <td>{card.mental}</td>
+                    <td>{card.Fatigue}</td>
+                    <td>{card.type1}</td>
+                    <td>{card.type2}</td>
+                    <td>{card.type3}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
-
-      <h1>練習一覧</h1>
-
-      <table border={1}>
-        <tbody>
-          <tr>
-            <th>練習名</th>
-            <th>キック</th>
-            <th>スピード</th>
-            <th>体力</th>
-            <th>テクニック</th>
-            <th>フィジカル</th>
-            <th>ジャンプ</th>
-            <th>メンタル</th>
-            <th>疲労度</th>
-            <th>タイプ1</th>
-            <th>タイプ2</th>
-            <th>タイプ3</th>
-          </tr>
-          {getMenuData.map((MenuData, idx) => (
-            <tr key={idx}>
-              <td>{MenuData.menu}</td>
-              <td>{MenuData.kick}</td>
-              <td>{MenuData.speed}</td>
-              <td>{MenuData.energy}</td>
-              <td>{MenuData.technique}</td>
-              <td>{MenuData.physical}</td>
-              <td>{MenuData.jump}</td>
-              <td>{MenuData.mental}</td>
-              <td>{MenuData.Fatigue}</td>
-              <td>{MenuData.type1}</td>
-              <td>{MenuData.type2}</td>
-              <td>{MenuData.type3}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
     </div>
   );
 }
